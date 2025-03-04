b5891b7610caee5b102e298eeb11de4dbaebefdf
iditenahuyvse <dleonra23@gmail.com>
2025-03-04 18:11:16 +0100

    db con room titulo + estreno

--- app/src/main/java/com/example/proyecto1raentrega/models/Pelicula.java
+++ app/src/main/java/com/example/proyecto1raentrega/models/Pelicula.java
@@ -1,4 +1,25 @@
 package com.example.proyecto1raentrega.models;
-
 public class Pelicula {
+    private String titulo;
+    private int estreno;
+
+    public Pelicula(){};
+    public Pelicula(String titulo, int estreno) {
+        this.titulo = titulo;
+        this.estreno = estreno;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getEstreno() {
+        return estreno;
+    }
+    public void setEstreno(int estreno) {
+        this.estreno = estreno;
+    }
 }
